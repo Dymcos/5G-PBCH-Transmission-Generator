ccb565e5047bc28ba5cd57a821a1f6bca7aaff91
Valentin Dymchenko <dymcos2@gmail.com>
2025-04-24 19:38:10 +0300

    example.m is updated

--- example.m
+++ example.m
@@ -14,10 +14,10 @@ symbolOffset = 0;
 subcarrierOffset = (NCRBSSB*12+kSSB)*2^-config.mu; % subcarrier offset
 NGridStart = 0; % the number of common resource block where the grid starts
 
-fs = 60e6;
+fs = 50e6;
 
 %% Create frame
-rg = createPbchFrame('C', 4, channelBandwidth, NCellId, MIB, SFN, symbolOffset, NCRBSSB, kSSB,[1 0.9 0.8 0.7]);
+rg = createPbchFrame('B', 4, channelBandwidth, NCellId, MIB, SFN, symbolOffset, NCRBSSB, kSSB,[1 0.9 0.8 0.7]);
 
 %% Resource grid painting
 figure
@@ -33,4 +33,13 @@ waveform = ofdmSignalGenerator(fs,rg(:,1:280),channelBandwidth,NGridStart,config
 %% Signal painting
 figure Name OFDM
 
-plot(real(waveform));
+samples = 1:length(waveform);
+plot(samples, real(waveform));
+tickShift = floor(length(waveform)/(50));
+tickVals = tickShift * (0:floor(length(waveform)/tickShift));
+xticks(tickVals)
+xValsPrecision = 0.001;
+xValsUnits = 1e-3; % for ms
+xticklabels(num2str([ceil((tickVals/fs / xValsUnits / xValsPrecision).') * xValsPrecision]))
+xlabel('t, ms')
+ylabel('Re[s(t)]');
